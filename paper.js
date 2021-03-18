654f3c98765fe411a2db67d47b79017061087d92
leena-dev <72690220+leena-dev@users.noreply.github.com>
2021-03-18 13:59:18 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,42 @@
+class paper
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+			restitution:0.3,
+			friction:0,
+			density:0.3
+
+			
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.image=loadImage("paper.png")
+		
+		this.body=Bodies.circle(this.x, this.y, this.r, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var paperpos=this.body.position;		
+
+			push();
+			translate(paperpos.x, paperpos.y);
+			strokeWeight(3);
+			fill(255,0,255);
+			ellipseMode(RADIUS);
+		//	ellipse(0,0,this.r, this.r);
+		imageMode(CENTER);
+		image(this.image, 0, 0, this.r*2, this.r*2)
+		
+
+			pop();
+			
+	}
+
+
+}
